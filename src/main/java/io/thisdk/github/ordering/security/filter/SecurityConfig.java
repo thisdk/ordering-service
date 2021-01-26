d55e72a77e874bad4dce50db87ec1aa95807c64f
JayHu <931998789@qq.com>
2021-01-26 22:41:03 +0800

    提交代码

--- src/main/java/io/thisdk/github/ordering/security/filter/SecurityConfig.java
+++ src/main/java/io/thisdk/github/ordering/security/filter/SecurityConfig.java
@@ -88,13 +88,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 验证码captchaImage 允许匿名访问
-                .antMatchers("/cms/user/login","/program/**").anonymous()
-                .antMatchers(
-                        "/*.html",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js"
-                ).permitAll()
+                .antMatchers("/cms/user/login","/program/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
                 .and()
