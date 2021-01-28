b9ef9a6e772555a5587b9ac6f4a7d673d069f882
thisdk <thisdk@gmail.com>
2021-01-29 00:44:36 +0800

    修复序列化

--- src/main/kotlin/io/thisdk/github/ordering/bean/OrderFood.kt
+++ src/main/kotlin/io/thisdk/github/ordering/bean/OrderFood.kt
@@ -1,9 +1,11 @@
 package io.thisdk.github.ordering.bean
 
+import java.io.Serializable
+
 data class OrderFood(
     val foodId: String,
     val foodName: String,
     val thumb: String,
     val price: Int,
     val quantity:Int,
-)
+) : Serializable
