eb1ee7088f8b11dec578cd2b255d4055031aef36
thisdk <thisdk@gmail.com>
2021-02-03 01:12:52 +0800

    删除时区配置

--- src/main/kotlin/io/thisdk/github/ordering/OrderingApplication.kt
+++ src/main/kotlin/io/thisdk/github/ordering/OrderingApplication.kt
@@ -3,13 +3,11 @@ package io.thisdk.github.ordering
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
-import java.util.*
 
 @SpringBootApplication
 @EnableCaching
 class OrderingApplication
 
 fun main(args: Array<String>) {
-    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"))
     runApplication<OrderingApplication>(*args)
 }
